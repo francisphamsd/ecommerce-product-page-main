159d5f52edcb5711ce88b801f95228767523acc1
Francis Pham <fphamsd@gmail.com>
2021-12-17 17:09:40 -0800

    finished header

--- script/main.js
+++ script/main.js
@@ -2,6 +2,11 @@ const hamburgerBtn = document.getElementById("hamburger-icon");
 const sideNav = document.getElementById("side-nav");
 const closeBtn = document.getElementById("close-icon");
 
+const carousel = document.querySelector(".carousel__track");
+const slides = [...carousel.children];
+console.log(slides);
+// Hamburger button logic
+
 hamburgerBtn.addEventListener("click", () => {
   sideNav.classList.remove("hidden");
 });
@@ -9,3 +14,11 @@ hamburgerBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sideNav.classList.add("hidden");
 });
+
+// Carousel logic
+
+// Arrange the slides next to one another
+
+// Slide left when click left
+// Slide right when click right
+// Click nav indicator, move to that slide
